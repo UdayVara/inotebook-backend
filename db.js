eb4e6a8477e4c9f52c84f6d30af08220b6243453
Uday vara <varaudayd@gmail.com>
2023-10-31 18:03:35 +0530

    added atlas

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectToMongo = () => {mongoose.connect('mongodb://localhost:27017/inotebook',
-  {
-    useUnifiedTopology: true
-  }
-)};
+const connectToMongo = () => { mongoose.connect('mongodb+srv://uday:uday4268@cluster0.ov7y3cz.mongodb.net/inotebook'
+).then(()=>{console.log("connection established")}).catch(()=>{console.log("failed to connect")})};
 
 module.exports = connectToMongo;
